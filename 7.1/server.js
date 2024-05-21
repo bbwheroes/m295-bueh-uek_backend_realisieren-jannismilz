0bd2af2ccc741a9e99fc3fd9a119ac9f43a12c25
Jannis Milz <jannis@milz.ch>
2024-05-21 09:37:13 +0200

    Refactor basic auth task

--- 7.1/server.js
+++ 7.1/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const basicAuth = require("express-basic-auth");
 const app = express();
 const port = 3000;
 
@@ -11,17 +10,26 @@ app.get("/public", (request, response) => {
 const credentials = {};
 credentials[process.env.AUTH_USER] = process.env.AUTH_PASSWORD;
 
-app.get(
-    "/private",
-    basicAuth({ 
-        users: credentials,
-        challenge: true,
-        realm: "Imb4T3st4pp",
-    }),
-    (request, response) => {
-        response.send("Hey, you're authenticated!");
+app.get("/private", (request, response) => {
+    if (!request.headers.authorization) {
+        return response
+            .status(401)
+            .header({
+                "WWW-Authenticate": 'Basic realm="Authenticate yourself!"',
+            })
+            .send();
     }
-);
+
+    const credentials = atob(request.headers.authorization.replace("Basic ", "")).split(":");
+    if (
+        credentials[0] === process.env.AUTH_USER ||
+        credentials[1] === process.env.AUTH_PASSWORD
+    ) {
+        return response.send("Hey, you're authenticated!");
+    }
+
+    return response.sendStatus(401);
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
